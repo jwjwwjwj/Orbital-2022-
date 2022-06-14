93491791b2cf4efa176b5d1a86b2daec7eaa4ae9
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-06-14 17:13:23 +0800

    Added side-by-side divs
    Added side-by-side div placeholders.

--- Orbital 0519/src/views/ReadFleet.vue
+++ Orbital 0519/src/views/ReadFleet.vue
@@ -1,5 +1,40 @@
 <template>
   <main class="read-fleet">
-    <h1>Fleet Overview</h1>
+    <h1><strong>Fleet Overview</strong></h1>
+    <div class="flex-container">
+      <div class="flex-child road-tax">
+        <!--span>This is for road-tax</span-->
+        <RoadTax />
+      </div>
+      <div class="flex-child vehicle-maintenance">
+        <span>This is for vehicle maintenance</span>
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+import RoadTax from "../components/RoadTax.vue";
+
+export default {
+  components: {
+    RoadTax,
+  },
+};
+</script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+}
+
+.flex-child {
+  flex: 2;
+  border: 2px solid grey;
+  margin: 10px;
+}
+
+.flex-child:first-child {
+  margin-right: 20px;
+}
+</style>
