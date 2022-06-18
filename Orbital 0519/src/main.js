a46bb32448b7d0860b69d8068d97b6a104bc040d
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-06-19 00:43:55 +0800

    Update main.js

--- Orbital 0519/src/main.js
+++ Orbital 0519/src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { DatePicker } from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'
+import 'ant-design-vue/dist/antd.css';
+import { Quasar } from "quasar";
+import iconSet from "quasar/icon-set/fontawesome-v6";
 
-createApp(App).use(DatePicker).use(Antd).use(store).use(router).mount('#app')
+createApp(App).use(DatePicker).use(Antd).use(store).use(router).use(Quasar).use(iconSet).mount('#app')
