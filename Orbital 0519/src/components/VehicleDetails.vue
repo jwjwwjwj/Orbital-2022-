9b6a62e15c72d8e112fae8a0d2417474810e96b2
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-06-20 02:39:11 +0800

    Changed formatting for table entries
    Changed formatting and added background colour to table header

--- Orbital 0519/src/components/VehicleDetails.vue
+++ Orbital 0519/src/components/VehicleDetails.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="q-pa-md">
     <q-table
+      :dense="$q.screen.lt.md"
       :rows="vehicles"
       :columns="columns"
       color="primary"
       row-key="name"
       :loading="loading"
+      :table-header-style="{ backgroundColor: '#D3D3D3' }"
+      table-header-class="text-bold"
     >
       <template v-slot:loading>
         <q-inner-loading showing color="primary" />
@@ -26,38 +29,55 @@ const columns = [
     align: "center",
     label: "Licence Plate",
     field: "licencePlate",
+    headerStyle: {
+      fontSize: "1.2em",
+    },
     sortable: true,
   },
   {
     name: "capacity",
+    align: "center",
     label: "Capacity",
     field: "capacity",
+    headerStyle: {
+      fontSize: "1.2em",
+    },
     sortable: true,
   },
   {
     name: "nextServicingDate",
+    align: "center",
     label: "Next Servicing Date",
     field: "nextServicingDate",
+    headerStyle: {
+      fontSize: "1.2em",
+    },
     format: (val) => moment(val.toDate()).format("DD MMMM YYYY"),
     sortable: true,
   },
   {
     name: "insuranceRenewalDate",
+    align: "center",
     label: "Insurance Renewal Date",
     field: "nextInsuranceRenewalDate",
+    headerStyle: {
+      fontSize: "1.2em",
+    },
     format: (val) => moment(val.toDate()).format("DD MMMM YYYY"),
     sortable: true,
   },
   {
     name: "roadTaxDueDate",
+    align: "center",
     label: "Road Tax Due Date",
     field: "roadTaxDueDate",
+    headerStyle: {
+      fontSize: "1.2em",
+    },
     format: (val) => moment(val.toDate()).format("DD MMMM YYYY"),
     sortable: true,
   },
 ];
-
-//const rows = [];
 
 export default {
   name: "VehicleDetails",
@@ -78,7 +98,6 @@ export default {
         vehicleData.id = vehicle.id;
         vehicles.push(vehicleData);
       });
-      console.log(vehicles);
       this.vehicles = vehicles;
     },
 
@@ -95,7 +114,6 @@ export default {
     },
 
     vehicleInsuranceAlert: (date) => {
-      console.log(moment().diff(moment(date), "months"));
       return (
         moment().diff(moment(date), "months") <= 1 &&
         moment().diff(moment(date), "months") >= 0
@@ -111,7 +129,6 @@ export default {
     return {
       loading: ref(false),
       columns,
-      //rows,
     };
   },
 };
