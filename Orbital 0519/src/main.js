035edb7d753d947ec2d18d22a835a00c442ee201
JW <52122631+jwjwwjwj@users.noreply.github.com>
2022-06-20 15:10:36 +0800

    Squashed commit of the following:
    commit b3616d946f0a04033e9676fdb1fc6d3f4a9d4a3c
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Mon Jun 20 00:41:12 2022 +0800
    
        Update Booking.vue
    
        removed rules first as it does not work
    
    commit fa34e85e345b4ec4b0b6fb700b23c10ea25f9b11
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Sun Jun 19 21:22:56 2022 +0800
    
        Connected the backend
    
        Submit button will submit the relevant data to Firestore database
    
    commit 1f324d9c5fc94d3c8836a8e398283527c17a75a1
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Sat Jun 18 15:40:37 2022 +0800
    
        fixed alignment
    
        updated alignment of the form
    
    commit e6316bee0261557852772fd9f6841a76c274fac1
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Tue Jun 14 17:13:35 2022 +0800
    
        Update App.vue
    
        incorporated the updated Nav Bar
    
    commit 513f0589e4b34ac55f25052d256383207fae26ca
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Tue Jun 14 17:05:47 2022 +0800
    
        updated Booking and login
    
        set disabled dates for the form
    
    commit 529be2362e16bad3743d9c9f8216c2546e315900
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Tue Jun 14 16:18:26 2022 +0800
    
        Update Booking.vue
    
        Fixed 1 and 2 way Button + view
    
    commit 0a06a8fede6326d8b47b01c4ddd4d21598561f44
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Tue Jun 14 00:53:45 2022 +0800
    
        Update Booking.vue
    
        radio button for 1 & 2 way + submit button
    
    commit b33349cefefdebaadff6d8993c2cbd3399413180
    Author: JW <52122631+jwjwwjwj@users.noreply.github.com>
    Date:   Wed Jun 8 16:43:28 2022 +0800
    
        Front End Booking form adjustments
    
        Touched up on the booking form

--- Orbital 0519/src/main.js
+++ Orbital 0519/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { DatePicker } from 'ant-design-vue';
-import Vuelidate from 'vuelidate'
 import 'ant-design-vue/dist/antd.css'
 
-createApp(App).use(Vuelidate).use(DatePicker).use(Antd).use(store).use(router).mount('#app')
+createApp(App).use(DatePicker).use(Antd).use(store).use(router).mount('#app')
