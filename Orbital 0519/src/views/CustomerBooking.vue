5144edfd4ea1474e36539fdb794868dd5a0338af
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-06-26 12:50:11 +0800

    Create CustomerBooking.vue
    Created customer booking view

---
+++ Orbital 0519/src/views/CustomerBooking.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="customer-booking">
+    <h1><strong>All Bookings</strong></h1>
+    <ViewBooking />
+  </main>
+</template>
+
+<script>
+import ViewBooking from "../components/ViewBooking.vue";
+
+export default {
+  components: {
+    ViewBooking,
+  },
+};
+</script>
