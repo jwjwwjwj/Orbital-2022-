de68d34ea5d954165e91dc611f1aa9ed93a510e0
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-06-26 12:50:40 +0800

    Create CustomerBookingHistory.vue
    Created customer booking history view

---
+++ Orbital 0519/src/views/CustomerBookingHistory.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="customer-booking">
+    <h1><strong>All Bookings</strong></h1>
+    <CustomerHistory />
+  </main>
+</template>
+
+<script>
+import CustomerHistory from "../components/CustomerHistory.vue";
+
+export default {
+  components: {
+    CustomerHistory,
+  },
+};
+</script>
