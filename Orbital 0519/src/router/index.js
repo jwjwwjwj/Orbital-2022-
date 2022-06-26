c681c1b6f1d9d55db4c87105c36c396de7f6f0c2
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-06-26 12:38:56 +0800

    Update index.js
    Removed duplicate import statements.

--- Orbital 0519/src/router/index.js
+++ Orbital 0519/src/router/index.js
@@ -7,7 +7,6 @@ import Fleet from "../components/Fleet.vue";
 import AddFleet from "../views/AddFleet.vue";
 import ReadFleet from "../views/ReadFleet.vue";
 import UpdateFleet from "../views/UpdateFleet.vue";
-import AddFleet from "../views/AddFleet.vue";
 import { auth } from "../firebase";
 
 const routes = [
