3dae10d22e6f19495fe25bbc6530b0981c2ff950
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-07-16 02:36:15 +0800

    Update Login.vue
    Updated the colour of the login button to black.

--- Orbital 0519/src/views/Login.vue
+++ Orbital 0519/src/views/Login.vue
@@ -1,121 +1,148 @@
 <template>
-	<main class="login">
-		<section class="forms">
-			<form class="login" @submit.prevent="login">
-				<h2>Login</h2>
-				<div class = "loginDetails">
-				<input 
-					type="email" 
-					placeholder="Email address"
-					v-model="login_form.email" />
-				<input 
-					type="password" 
-					placeholder="Password" 
-					v-model="login_form.password" />
-
-				<input 
-					type="submit" 
-					value="Login" />
-				</div>
-			</form>
-
-		</section>
-	</main>
+  <main class="login">
+    <h1 style="margin-top: 40px">TRANSPORFFICIENT</h1>
+    <section class="forms">
+      <form class="login-form" @submit.prevent="login">
+        <h2><strong>Login</strong></h2>
+        <div class="input-container">
+          <div class="input-child-email">
+            <input
+              class="form-input"
+              type="email"
+              placeholder="Email address"
+              v-model="login_form.email"
+            />
+          </div>
+          <div class="input-child-password">
+            <input
+              class="form-input"
+              type="password"
+              placeholder="Password"
+              v-model="login_form.password"
+            />
+          </div>
+          <div class="input-child-button">
+            <input
+              type="submit"
+              value="Login"
+              class="form-input"
+              style="background-color: rgba(0, 0, 0)"
+            />
+          </div>
+        </div>
+      </form>
+    </section>
+  </main>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { useStore } from 'vuex'
+import { ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
-	setup () {
-		const login_form = ref({});
-		const register_form = ref({});
-		const store = useStore();
+  setup() {
+    const login_form = ref({});
+    const register_form = ref({});
+    const store = useStore();
 
-		const login = () => {
-			store.dispatch('login', login_form.value);
-		}
+    const login = () => {
+      store.dispatch("login", login_form.value);
+    };
 
-		const register = () => {
-			store.dispatch('register', register_form.value);
-		}
+    const register = () => {
+      store.dispatch("register", register_form.value);
+    };
 
-		return {
-			login_form,
-			register_form,
-			login,
-			register
-		}
-	}
-}
+    return {
+      login_form,
+      register_form,
+      login,
+      register,
+    };
+  },
+};
 </script>
 
 <style>
+main,
+section {
+  text-align: center;
+}
+
 .forms {
-	display: flex;
-	min-height: 100vh;
+  display: flex;
+  text-align: center;
+  min-height: 100vh;
+}
+
+.login-form,
+.login {
+  display: block;
+  text-align: center;
+  align-items: center;
 }
 
 form {
-	flex: 1 1 0%;
-	padding: 8rem 1rem 1rem;
+  flex: 1 1 0%;
+  padding: 8rem 1rem 1rem;
+  text-align: center;
 }
 
 h2 {
-	font-size: 2rem;
-	text-transform: uppercase;
-	margin-bottom: 2rem;
+  font-size: 2rem;
+  text-transform: uppercase;
+  margin-bottom: 2rem;
+}
+
+.form-input {
+  display: block;
+}
+
+main section form div.input-container,
+main section form div.input-container div.input-child-email,
+main section form div.input-container div.input-child-password,
+main section form div.input-container div.input-child-button {
+  text-align: center;
 }
 
 input {
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
-	text-align: center;
-	display: block;
-	width: 100%;
-	max-width: 900px;
-	margin: 0 auto;
-	font-size: 1.5rem;
-	margin-bottom: 2rem;
-	padding: 0.5rem 0rem;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  display: block;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+  padding: 0.5rem 0rem;
 }
 
-.loginDetails {
-	text-align: center;
-	display: inline-block;
-	max-width: 900px;
-	margin: 0 auto;
-	font-size: 1.5rem;
-	margin-bottom: 2rem;
-	padding: 0.5rem 0rem;
-}
 input:not([type="submit"]) {
-	opacity: 0.8;
-	transition: 0.4s;
+  opacity: 0.8;
+  transition: 0.4s;
 }
 
 input:focus:not([type="submit"]) {
-	opacity: 1;
+  opacity: 1;
 }
 
 input::placeholder {
-	color: inherit;
+  color: inherit;
 }
-form.login input:not([type="submit"]) {
-	color: #2c3e50;
-	border-bottom: 2px solid #2c3e50;
+form.login-form input:not([type="submit"]) {
+  color: #2c3e50;
+  border-bottom: 2px solid #2c3e50;
 }
 
-form.login input[type="submit"] {
-	background-color: rgb(245, 66, 101);
-	color: #FFF;
-	font-weight: 700;
-	padding: 1rem 2rem;
-	border-radius: 0.5rem;
-	cursor: pointer;
-	text-transform: uppercase;
+form.login-form input[type="submit"] {
+  background-color: rgb(245, 66, 101);
+  color: #FFF;
+  font-weight: 700;
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  text-transform: uppercase;
 }
 </style>
