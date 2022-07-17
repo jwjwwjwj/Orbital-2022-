a27e60d4f4fac789a352c8fc12d18e578e8ebe3d
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-07-17 23:06:34 +0800

    Added rain background (#18)
    * Add files via upload
    
    * Update Home.vue
    
    Added an additional rain background.

--- Orbital 0519/src/views/Home.vue
+++ Orbital 0519/src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <div id="home" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+  <div
+    id="home"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16
+        ? 'warm'
+        : typeof weather.main != 'undefined' &&
+          weather.weather[0].main === 'Rain'
+        ? 'rain'
+        : ''
+    "
+  >
     <div class="words" style="text-align: center">
-    <h1><strong>Home</strong></h1>
+      <h1><strong>Home</strong></h1>
     </div>
     <main class="weather">
       <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
+        <input
+          type="text"
+          class="search-bar"
           placeholder="Search..."
           v-model="query"
           @keypress="fetchWeather"
@@ -16,7 +26,9 @@
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
 
@@ -31,50 +43,77 @@
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      api_key: '56335ecb5a4bacf3b82faa7c96847d66',
-      url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: 'Singapore',
-      weather: {}
-    }
+      api_key: "56335ecb5a4bacf3b82faa7c96847d66",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "Singapore",
+      weather: {},
+    };
   },
   methods: {
-    fetchWeather (e) {
-      if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then(res => {
+    fetchWeather(e) {
+      if (e.key === "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
             return res.json();
-          }).then(this.setResults);
+          })
+          .then(this.setResults);
       }
     },
     defaultWeather() {
-          if (this.query === "Singapore") {
-      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then(res => {
+      if (this.query === "Singapore") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
             return res.json();
-          }).then(this.setResults);
+          })
+          .then(this.setResults);
       }
     },
-    setResults (results) {
+    setResults(results) {
       this.weather = results;
     },
-    dateBuilder () {
+    dateBuilder() {
       let d = new Date();
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
       let day = days[d.getDay()];
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
       return `${day} ${date} ${month} ${year}`;
-    }
+    },
   },
   created() {
     this.defaultWeather();
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -84,17 +123,20 @@ export default {
   box-sizing: border-box;
 }
 body {
-  font-family: 'montserrat', sans-serif;
+  font-family: "montserrat", sans-serif;
 }
 #home {
-  background-image: url('/src/assets/cold-weather.jpg');
+  background-image: url("/src/assets/cold-weather.jpg");
   min-height: 100vh;
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
 #home.warm {
-  background-image: url('/src/assets/sunny-weather.jpg');
+  background-image: url("/src/assets/sunny-weather.jpg");
+}
+#home.rain {
+  background-image: url("/src/assets/rain.gif");
 }
 #home.words {
   text-align: center;
@@ -103,7 +145,11 @@ body {
 #weather {
   min-height: 100vh;
   padding: 25px;
-  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
 }
 .search-box {
   width: 100%;
@@ -115,11 +161,11 @@ body {
   display: block;
   width: 100%;
   padding: 15px;
-  
+
   color: #313131;
   font-size: 20px;
   appearance: none;
-  border:none;
+  border: none;
   outline: none;
   background: none;
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
@@ -156,7 +202,7 @@ body {
   font-size: 102px;
   font-weight: 900;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color:rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
   box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
