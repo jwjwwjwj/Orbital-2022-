4c1081db0c2f6818be45e0048a55b0534eeb3576
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-07-20 16:50:37 +0800

    Update main.js
    Removed commented lines for styling test.

--- Orbital 0519/src/main.js
+++ Orbital 0519/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-//import Antd from 'ant-design-vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//import { DatePicker } from 'ant-design-vue';
-//import 'ant-design-vue/dist/antd.css'
+import { DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'
 import { Quasar } from "quasar";
 import iconSet from "quasar/icon-set/fontawesome-v6";
 
-createApp(App)./*use(DatePicker).use(Antd).*/use(store).use(router).use(Quasar).use(iconSet).mount('#app')
+createApp(App).use(DatePicker).use(Antd).use(store).use(router).use(Quasar).use(iconSet).mount('#app')
