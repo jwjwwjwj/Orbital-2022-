e7cb24c2b8b8fe8277fcf3fcc05b454478ff04c0
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-07-22 17:58:38 +0800

    Create PageNotFound.vue
    Created a "Page 404" component.

---
+++ Orbital 0519/src/components/PageNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="center">
+    <h2>
+      <strong>The page you are accessing does not exist.</strong>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
+
+<style scoped>
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px;
+  text-align: center;
+}
+</style>
