b6336b3c6c04e39a3cf2a90f2d52f61d3ecbf7ad
e0727131 <97497636+e0727131@users.noreply.github.com>
2022-07-22 19:02:27 +0800

    Update Home.vue
    Added a comma to separate day from date.

--- Orbital 0519/src/views/Home.vue
+++ Orbital 0519/src/views/Home.vue
@@ -113,7 +113,7 @@ export default {
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
-      return `${day} ${date} ${month} ${year}`;
+      return `${day}, ${date} ${month} ${year}`;
     },
   },
   created() {
